3009ce39dc1ebff670a4443c1d19a27d8176a953
lluanagabrieli <luanagabrieli4599@hotmail.com>
2025-06-12 00:20:58 -0300

    feat: adiciona formulário para adicionar produtos e listagem de produtos

--- src/app/pages/produtos/produtos.component.ts
+++ src/app/pages/produtos/produtos.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
 export class ProdutosComponent {
+  listaProdutos: { nome: string, categoria: string, preco: number }[] = [];
 
+  produto = {
+    nome: '',
+    categoria: '',
+    preco: 0
+  };
+
+  salvarProduto() {
+    this.listaProdutos.push({...this.produto});
+
+    this.produto = {
+      nome: '',
+      categoria: '',
+      preco: 0
+    };
+  }
 }
